1b12fb6f3d3741c2cc28dd08a65e1407013bac33
Angelica Serna <angelicasernah@gmail.com>
2021-04-23 15:58:33 -0500

    Entidades y formularios del modulo Facturacion

---
+++ FRONT/ENDDD/forms/T_FRONTTOIWYMVS_510/events/executeCommand/VA_VABUTTONFKUAKJO_460877.js
@@ -0,0 +1,11 @@
+
+
+//Entity: Factura
+//Factura. (Button) View: FacturacionForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONFKUAKJO_460877 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.Factura = true;
+
+};
---
+++ FRONT/ENDDD/forms/T_FRONTTOIWYMVS_510/events/executeQuery/Q_DETACRUA_DQ58.js
@@ -0,0 +1,6 @@
+
+//DetalleFacturaQuery Entity: DetalleFactura
+task.executeQuery.Q_DETACRUA_DQ58 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.DetalleFactura = true;
+};
